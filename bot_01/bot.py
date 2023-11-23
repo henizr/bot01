c585ebf257689108109d7efc892519af2b5f07c9
palindrom8 <andr.nitgrafika@yandex.ru>
2023-11-24 01:57:48 +0300

    Modified bot01

--- bot_01/bot.py
+++ bot_01/bot.py
@@ -6,10 +6,20 @@ API_TOKEN = '6427191316:AAEUI2mYgRhWkzogKn7lo4vUMCOUwu-gnto'
 bot = telebot.TeleBot(API_TOKEN)
 
 
-@bot.message_handler()
+@bot.message_handler(commands=["start"])
 def start(message):
-    bot.reply_to(message, "Hello!")
-    # print("Hi")
+    bot.reply_to(message, "Hi! I`m a bot.")
 
+@bot.message_handler(commands=["hello"])
+def greeting(message):
+    bot.reply_to(message, "Hello, my dear friend! I`m glad to see you!")
+
+
+@bot.message_handler(content_types=['text'])
+def answer(message):
+
+    text = message.text
+
+    bot.reply_to(message,f"Ты отправил(а) сообщение: '{text}'")
 
 bot.polling(none_stop=True, interval=0)
