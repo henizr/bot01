3b9e97738860242080c0ac4b7965ddb6d32fdf84
Harry <henry.iz@yandex.ru>
2023-11-24 00:48:07 +0300

    Added bot01

--- bot_01/bot.py
+++ bot_01/bot.py
@@ -1,5 +1,15 @@
 import telebot
 
-API_TOKEN = '<api_token>'
+
+API_TOKEN = '6427191316:AAEUI2mYgRhWkzogKn7lo4vUMCOUwu-gnto'
 
 bot = telebot.TeleBot(API_TOKEN)
+
+
+@bot.message_handler()
+def start(message):
+    bot.reply_to(message, "Hello!")
+    # print("Hi")
+
+
+bot.polling(none_stop=True, interval=0)
